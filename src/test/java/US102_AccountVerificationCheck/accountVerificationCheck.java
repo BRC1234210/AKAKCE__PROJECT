c76be4c9e663b58861655121b8d2cce646431f06
iahmedkoc <i.ahmedkoc@gmail.com>
2025-10-08 16:12:54 +0300

    AccountVerificationCheck Tamamlandi. Base Driver Guncellendi.

--- src/test/java/US102_AccountVerificationCheck/accountVerificationCheck.java
+++ src/test/java/US102_AccountVerificationCheck/accountVerificationCheck.java
@@ -4,61 +4,52 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import utility.BaseDriver;
 
 import java.time.Duration;
 
 public class accountVerificationCheck {
     public static void main(String[] args) {
-        WebDriver chromeDriver=new ChromeDriver();
-        chromeDriver.get("https://www.akakce.com/");
-        chromeDriver.manage().window().maximize();
-        chromeDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-        WebDriverWait wait=new WebDriverWait(chromeDriver,Duration.ofSeconds(30));
+
+        WebDriver driver= BaseDriver.driver("https://www.akakce.com/");
+        WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(30));
 
         final String email="i.ahmedkoc@gmail.com";
         final String password="Maykop0101";
         final String expectedUserName="Ismail Ahmed";
 
-        WebElement girisyapBtn=chromeDriver.findElement(By.xpath("//div[@class='g-3_v8 hm_hp_v8']/div/a"));
-        wait.until(ExpectedConditions.elementToBeClickable(girisyapBtn));
+        WebElement girisyapBtn=driver.findElement(By.xpath("//div[@class='g-3_v8 hm_hp_v8']/div/a"));
         girisyapBtn.click();
 
-        WebElement emailInput=chromeDriver.findElement(By.xpath("//input[@class='email']"));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@class='email']")));
+        BaseDriver.threadWait(3);
+
+        WebElement emailInput=driver.findElement(By.xpath("//input[@class='email']"));
         wait.until(ExpectedConditions.elementToBeClickable(emailInput));
         emailInput.sendKeys(email);
 
-        WebElement devametBtn=chromeDriver.findElement(By.xpath("//div[@class='bc']/button/b"));
-        wait.until(ExpectedConditions.elementToBeClickable(devametBtn));
+        WebElement devametBtn=driver.findElement(By.xpath("//div[@class='bc']/button/b"));
         devametBtn.click();
 
-        WebElement passwordInput=chromeDriver.findElement(By.xpath("//input[@type='password']"));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@type='password']")));
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@type='password']")));
-        passwordInput.click();
+        WebElement passwordInput=driver.findElement(By.xpath("//input[@type='password']"));
         passwordInput.sendKeys(password);
-        passwordInput.click();
 
-        WebElement parolagosterBtn=chromeDriver.findElement(By.xpath("//i[@class='e']"));
-        parolagosterBtn.click();
-
-        WebElement girisYapBtn2=chromeDriver.findElement(By.xpath("//div[@class='bc']/button"));
+        WebElement girisYapBtn2=driver.findElement(By.xpath("//div[@class='bc']/button"));
         girisYapBtn2.click();
 
-        WebElement userNameText=chromeDriver.findElement(By.xpath("//div/a/i"));
+        WebElement userNameText=driver.findElement(By.xpath("//div[@class='g-3_v8 hm_hp_v8']/div/a/i"));
         wait.until(ExpectedConditions.visibilityOf(userNameText));
 
         final String actualText=userNameText.getText();
 
-        Assert.assertEquals("Username uyumsuz!",expectedUserName,actualText);
+        Assert.assertEquals("Username uyumsuz!", expectedUserName, actualText);
 
-        chromeDriver.quit();
-
-
+        driver.quit();
 
 
     }
 }
+
+
+
